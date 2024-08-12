dc1551d2db725dbdca990726419148d524d2dbeb
John Lynch <johnf.lynch@yahoo.ie>
2024-08-12 14:19:29 +0000

    add static files to settings

--- main/settings.py
+++ main/settings.py
@@ -177,8 +177,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Cloudinary Settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
