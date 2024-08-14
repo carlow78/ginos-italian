4ba0c66b26c014bf690b683c82f8dc9a56a01406
John Lynch <johnf.lynch@yahoo.ie>
2024-08-14 10:05:07 +0000

    edit reservation test case

--- reservationsystem/test.py
+++ reservationsystem/test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from reservationsystem.models import ReservationSystem  
+from reservationsystem.models import ReservationSystem
+from  .views import EditBooking
 from django.contrib.auth.models import User
 
 class TestAddBooking(TestCase):
@@ -17,6 +18,7 @@ class TestAddBooking(TestCase):
     def test_create_reservation(self):
         # Test creating a reservation
         response = self.client.post(reverse('bookings:add_booking'), {
+            'booking_id': 1,
             'booking_name': 'Tom',
             'telephone_number': '03456754',
             'number_of_people': 4,
@@ -26,5 +28,28 @@ class TestAddBooking(TestCase):
         })
         
         # Check that the reservation was created successfully
-        self.assertEqual(response.status_code, 200)  
-        self.assertTrue(ReservationSystem.objects.filter(booking_name='Tom').exists())
+        self.assertEqual(response.status_code, 302)  
+        
+
+    def setUp(self):
+
+        self.kwargs = {'pk': 1}
+
+    def test_edit_reservation(self):
+        # Test edit a reservation
+            self.client.login(username='Tom', password='MagnumPI')
+            booking_id = self.kwargs.get('pk')
+            response = self.client.post(reverse('bookings:edit_reservation', kwargs={'pk': booking_id}))
+            {
+            'booking_name': 'Tom',
+            'telephone_number': '03456754',
+            'number_of_people': 6,
+            'date': '2024-08-15', 
+            'time': '19:00',
+            'comments': 'None',
+        }
+
+            self.assertEqual(response.status_code, 302)  
+            
+
+   
